44a71a62ae5ba4cbbbe15806cfacab81c563ae08
Sohan SM <sohansm@google.com>
2023-12-08 18:21:29 +0000

    pubsub receiver is working

--- pages/api/todos/listen.ts
+++ pages/api/todos/listen.ts
@@ -2,17 +2,20 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { TodoService } from '@/app/todo_service';
 import { Message } from '@/app/publish';
 import { buffer } from 'stream/consumers';
+import { ErrorResponse } from '@/app/models/error_response';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Object | Error>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Object | ErrorResponse>) {
   try {
-    const data = req.body.data as Buffer;
-    const message = JSON.parse(data.toString("base64")) as Message;
+    const data = Buffer.from(req.body.message.data, "base64").toString().trim();
+    const message = JSON.parse(data) as Message;
     await TodoService.process(message);
-    res.status(200).json({});
+    res.status(200).json(message);
   } catch (error) {
     if (error instanceof Error) {
-      res.status(500).json(error);
+      res.status(500).json({message: error.message});
+      return
     }
+    res.status(500).json({message: new Error(error as string)});
   }
 
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -8,7 +8,7 @@ variable "project" {
   default = "sohansm-project"
 }
 variable "image" {
-  default = "us-west1-docker.pkg.dev/sohansm-project/cloud-run-source-deploy/immerse-next@sha256:a4547477b69eced9ea4ed6a60f63cb1e3634db60bd56cd05b76dfbf58ceb67f7"
+  default = "us-west1-docker.pkg.dev/sohansm-project/cloud-run-source-deploy/immerse-next@sha256:3b3de27aee72467668f133e8f0362c36bce7c23b0afd24f0e61bdba34de055c4"
 }
 variable "mysql-user" {
   default = "immerse-db-user"
